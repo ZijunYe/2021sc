f202548a5f6409b77718931f2f56fa4c47f982ce
Joy Ye <zijunye001@gmail.com>
2021-05-03 16:55:24 -0400

    challenge01

---
+++ 01/app.js
@@ -0,0 +1,30 @@
+const cursor = document.querySelector('.cursor');
+
+window.addEventListener("mousemove",(e) =>{
+	cursor.style.left = e.pageX +'px';
+	cursor.style.top = e.pageY +'px';
+	//console.log(e.pageX,e.pageY)
+
+	cursor.setAttribute('data-fromTop',(cursor.offsetTop - scrollY))
+})
+
+
+window.addEventListener('scroll',()=>{
+
+	const fromTop = parseInt(cursor.getAttribute('data-fromTop'));
+	cursor.style.top = scrollY + fromTop +'px';
+	console.log(scrollY)
+
+})
+
+
+window.addEventListener('click',()=>{
+
+	if(cursor.classList.contains('click')){
+		cursor.classList.remove('click');
+		void cursor.offsetWidth;
+		cursor.classList.add('click')
+	}else{
+		cursor.classList.add('click')
+	}
+})
