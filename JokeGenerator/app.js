3cd5d90ee0ea4e098cc041eaa970670238e2473d
Joy Ye <zijunye001@gmail.com>
2021-05-05 18:56:18 -0400

    chanllenge3 done

---
+++ JokeGenerator/app.js
@@ -0,0 +1,34 @@
+const button = document.querySelector('.container button');
+
+const jokeText = document.querySelector('.container p');
+
+document.addEventListener('DOMContentLoaded', getJoke)
+
+
+
+
+
+button.addEventListener('click',getJoke);
+
+function getJoke(){
+	fetch('https://icanhazdadjoke.com/',{
+		headers:{
+			'Accept':'application/json'
+		}
+	}).then(data => data.json())
+	.then (obj => jokeText.innerHTML = obj.joke)
+}
+
+
+
+/*async function getJoke(){
+	const jokeData = await fetch('https://icanhazdadjoke.com/',{
+		headers:{
+			'Accept':'application/json'
+		}
+	});
+	const jokeObject = await jokeData.json();
+	jokeText.innerHTML = jokeObject.joke;
+	console.log(jokeOject.joke)
+}
+*/
